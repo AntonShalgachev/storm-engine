e61670491cceb69ef60889471de1115ab408ad92
Anton Shalgachev <anton.shalgachev@gmail.com>
2024-04-27 02:47:12 +0200

    add fmodex

--- SoundService/SoundService.h
+++ SoundService/SoundService.h
@@ -9,8 +9,8 @@
 #include "VSoundService.h"
 #include "VSound.h"
 #include "..\common_h\CVECTOR.h"
-#include ".\FMOD4\api\inc\fmod.hpp"
-#include ".\FMOD4\api\inc\fmod_errors.h"
+#include "fmod.hpp"
+#include "fmod_errors.h"
 #include "..\common_h\templates.h"
 
 
